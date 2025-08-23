2a945f27a834802984cef0703d74a9e459f58c75
Tom <tom@tomsteer.com>
2025-08-23 15:23:38 +0100

    Better dockerrun completions, fzf, editor

--- zsh/completions/dockerrun.sh
+++ zsh/completions/dockerrun.sh
@@ -3,15 +3,35 @@ then
 	exit 0
 fi
 
-_dockerrun_script()
-{
-  _script_commands=$(dr list | tail -n +2)
 
-  local cur
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -W "${_script_commands}" -- ${cur}) )
+# bash completion for dockerrun.sh
+_dockerrun_completions() {
+    local cur prev opts images
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-  return 0
+    # Flags
+    opts="--help --rebuild --list --mount"
+
+    # If the previous word is --mount, suggest directories
+    if [[ "$prev" == "--mount" ]]; then
+        COMPREPLY=( $(compgen -d -- "$cur") )
+        return 0
+    fi
+
+    # If no image yet (first non-flag argument), suggest available docker images
+    # (directories inside ~/scripts/dockerfiles)
+    if [[ ! " ${COMP_WORDS[*]} " =~ " --list " ]]; then
+        # collect list of images
+				images=$(compgen -W "$(dr --list | tail -n +2 | awk '{print $1}')" -- "$cur")
+    fi
+
+    # Add flags + images
+    COMPREPLY=( $(compgen -W "${opts} ${images}" -- "$cur") )
+    return 0
 }
-complete -F _dockerrun_script dr
+
+# Register completion
+complete -F _dockerrun_completions dockerrun dr
+
